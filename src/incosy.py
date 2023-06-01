3732afcb29f91b6f125eb6789faaed12f1b33b8a
Anbraten <anton@ju60.de>
2023-06-01 19:38:34 +0200

    improve memory usage

--- src/incosy.py
+++ src/incosy.py
@@ -1,10 +1,12 @@
 import yaml
 
 from langchain import LLMChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ReadOnlySharedMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.agents import initialize_agent, Tool, AgentType
+from langchain.chains import ConversationChain
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
 
 
 class SafeDict(dict):
@@ -35,6 +37,9 @@ Suggest products for the following problem of a caregiver of a nursing home as b
 
 {product_list}
 
+Current conversation:
+{chat_history}
+
 Use the following format:
 
 Question: the input problem to suggest products for
@@ -50,7 +55,68 @@ Begin!
 Question: {input}
 Thought:
     """.format_map(data).strip()
+    return prompt
 
+
+def get_product_description_prompt():
+    file = open("database.yml", "r")
+    database = yaml.load(file, Loader=yaml.FullLoader)
+
+    product_list = []
+    product_names = []
+    for product in database['products']:
+        if not 'use_cases' in product:
+            print("Product " + product.get('name') +
+                  " has no name or use_cases")
+            continue
+        product_names.append(product.get('name'))
+        product_list.append(product.get('name') + ": " +
+                            product.get('use_cases').replace('\n', ' ').replace('- ', ''))
+
+    data = SafeDict(product_list='- ' + '\n- '.join(product_list),
+                    product_names=', '.join(product_names))
+
+    prompt = """
+Explain how the caregiver can use the suggested products to solve their problem.
+
+Current conversation:
+{chat_history}
+
+Caregiver: {input}
+AI:
+    """.format_map(data).strip()
+    return prompt
+
+
+def get_product_buy_prompt():
+    file = open("database.yml", "r")
+    database = yaml.load(file, Loader=yaml.FullLoader)
+
+    product_list = []
+    product_names = []
+    for product in database['products']:
+        if not 'use_cases' in product:
+            print("Product " + product.get('name') +
+                  " has no name or use_cases")
+            continue
+        product_names.append(product.get('name'))
+        product_list.append(product.get('name') + ": " +
+                            product.get('use_cases').replace('\n', ' ').replace('- ', ''))
+
+    data = SafeDict(product_list='- ' + '\n- '.join(product_list),
+                    product_names=', '.join(product_names))
+
+    prompt = """
+Tell the caregiver that you will forward their suggestion to the home management.
+Tell the caregiver that you will also notify them again when the home management has decided to buy the product.
+Tell the caregiver that they can always ask you for help again and tell them that you care about them.
+
+Current conversation:
+{chat_history}
+
+Caregiver: {input}
+AI:
+    """.format_map(data).strip()
     return prompt
 
 
@@ -58,71 +124,59 @@ def open_chat():
     llm = ChatOpenAI(
         temperature=0, model_name='gpt-3.5-turbo', verbose=True)
 
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", output_key="output")
-    memory.chat_memory.add_ai_message(
+    chat_memory = ChatMessageHistory()
+    chat_memory.add_ai_message(
         "How was your last shift?")
+    memory = ConversationBufferMemory(
+        chat_memory=chat_memory, memory_key="chat_history", human_prefix="Caregiver:", output_key="output")
+    readonly_memory = ReadOnlySharedMemory(memory=memory)
 
-    product_qa = LLMChain(
-        llm=llm,
+    product_chain = ConversationChain(
         prompt=PromptTemplate(
             template=get_best_product_prompt(),
-            input_variables=["input"],
+            input_variables=["chat_history", "input"],
         ),
+        llm=llm,
+        verbose=True,
+        memory=readonly_memory,
     )
 
-    describe = LLMChain(
-        llm=llm,
+    describe_chain = ConversationChain(
         prompt=PromptTemplate(
-            template="""You are an AI assistant for caregivers of a nursing home. You are asked to describe a product that can help with a problem in the daily caregiving job.
-                You are given the following extracted parts of information about possible products.
-                If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-                Question: {input}
-                """,
-            # input_variables=["input", "chat_history", "agent_scratchpad"],
-            input_variables=["input"],
+            template=get_product_description_prompt(),
+            input_variables=["chat_history", "input"],
         ),
+        llm=llm,
+        verbose=True,
+        memory=readonly_memory,
     )
 
-    buy = LLMChain(
-        llm=llm,
+    buy_chain = LLMChain(
         prompt=PromptTemplate(
-            # template="""You are an AI assistant for caregivers of a nursing home. You are asked to suggest products that can help with a problem in the daily caregiving job.
-            # You are given the following extracted parts of information about possible products.
-            # If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-            # Question: {input}
-            # =========
-            # {chat_history}
-            # =========
-            # Answer:
-            # {agent_scratchpad}
-            # """,
-            template="""You are an AI assistant for caregivers of a nursing home. You are asked to suggest products that can help with a problem in the daily caregiving job.
-            You are given the following extracted parts of information about possible products.
-            If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-            Question: {input}
-            """,
-            # input_variables=["input", "chat_history", "agent_scratchpad"],
-            input_variables=["input"],
+            template=get_product_buy_prompt(),
+            input_variables=["chat_history", "input"],
         ),
+        llm=llm,
+        verbose=True,
+        memory=readonly_memory,
     )
 
     tools = [
         Tool(
             name="problem",
-            func=product_qa.run,
+            func=product_chain.run,
             description="""useful for when the caregiver is telling us about a problem they have in caregiving""",
             return_direct=True,
         ),
         Tool(
             name="describe",
-            func=describe.run,
+            func=describe_chain.run,
             description="""useful for when the caregiver is interested in a product and wants to know more about it""",
             return_direct=True,
         ),
         Tool(
             name="buy",
-            func=buy.run,
+            func=buy_chain.run,
             description="""useful for when the caregiver wants to suggest a product to the home lead""",
             return_direct=True,
         ),
